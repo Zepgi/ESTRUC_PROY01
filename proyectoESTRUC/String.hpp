ca0291bfc553eb706db3ee8cc3a0b94b6139d706
Eyden Su <137385937+Zepgi@users.noreply.github.com>
2023-08-22 21:18:08 -0600

    Update String.hpp

--- proyectoESTRUC/String.hpp
+++ proyectoESTRUC/String.hpp
@@ -1,31 +1,48 @@
+
 #ifndef STRING_HPP
 #define STRING_HPP
 
-class String {
-public:
-    String(char* pCadena);  // Constructor
-    ~String();  // Destructor
+/*****Datos administrativos************************
+ * Nombre del archivo: String.hpp
+ * Tipo de archivo: header (.hpp)
+ * Proyecto: 
+ * Autor: 
+ *  Eyden Su Díaz
+ *  Heldyis Aguero Espinoza
+ *  Mary Paz Alvarez Navarrete
+ * Empresa: TEC
+ *****Descripción**********************************
+ * declaraciones de clase y metodos y funciones del proyecto 
+ * 
+ *****Versión**************************************
+ * ## | Fecha y hora | Autor
+ *   22 AGO 2023, 5:23
+ **************************************************/
 
-    char caracterEn(int pIndice);
-    int contarCaracter(char pCaracter);
-    int ultimoIndice(char pCaracter);
+class String {
+
+private:
+    char *gpCadena;
+public:
+    String(char *pCadena);
+    ~String();
+
+    char caracterEn(int indice);
+    int contarCaracter(char caracter);
+    int ultimoIndice(char caracter);
     void cambiarCadena(const char* pNewCadena);
     int len();
     const char* obtenerCadena() const;
     bool equals(const char* pCadena);
 
-    String** split(char pDelimitador);
-    void concatenarEn(const char* pString, int pIndice);
+    String** split(char caracterDelimitador);
+    void concatenarEn(const char* pString, int indice);
     void concatenar(const char* pString);
-    void concatenarCadenas(const char** strArray);
-    void reemplazarEn(const char* pReemplazo, int pIndice);
+    void concatenarCadenas(const char* pNuevaCadena[]);
+    void reemplazarEn(const char* pReemplazo, int indice);
     void reemplazarOcurrencias(const char* pOcurrencia, const char* pReemplazo);
-    void guardarEnArchivo(const char* path, const char* mode);
-    void leerArchivo(const char* path);
-
-private:
-    char* cadena;  // Actual character array
+    void guardarEnArchivo(const char* pDireccion);
+    void leerArchivo(const char* pDirección);
 };
 
 #endif
-
